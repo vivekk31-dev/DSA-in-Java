539ee62f190a3b1f352e819583d42f3ef755a4d4
Vivek Singh <singhvivek2632@gmail.com>
2026-08-05 09:02:46 +0530

    Time: 14 ms (39.64%) | Memory: 47.5 MB (94.58%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,12 +1,12 @@
 class Solution {
 
-    public long CalculateTotalHour(int[] piles, int speed) {
-        long totalHour = 0;
-        for (int i = 0; i < piles.length; i++) {
-            totalHour += (long) Math.ceil((double) piles[i] / speed);
-        }
-        return totalHour;
-    }
+    // public long CalculateTotalHour(int[] piles, int speed) {
+    //     long totalHour = 0;
+    //     for (int i = 0; i < piles.length; i++) {
+    //         totalHour += (long) Math.ceil((double) piles[i] / speed);
+    //     }
+    //     return totalHour;
+    // }
 
     public int minEatingSpeed(int[] piles, int h) {
         int max = piles[0];
@@ -18,12 +18,25 @@ class Solution {
         int low = 1, high = max, ans = max;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            long totalH = CalculateTotalHour(piles, mid);
-            if (totalH <= h) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            long totalhour=0;
+            // long totalH = CalculateTotalHour(piles, mid);
+            for(int i=0;i<piles.length;i++){
+                totalhour+=piles[i]/mid;
+                if(piles[i]%mid!=0){
+                    totalhour+=1;
+                }
+            }
+            // if (totalH <= h) {
+            //     ans = mid;
+            //     high = mid - 1;
+            // } else {
+            //     low = mid + 1;
+            // }
+            if(totalhour>h){
+                low=mid+1;
+            }else if(totalhour<=h){
+                high=mid-1;
+                ans=mid;
             }
         }
         return ans;
