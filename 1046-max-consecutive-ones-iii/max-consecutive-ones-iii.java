0390567ba933f9360d01ccc82644a4a24294b143
Vivek Singh <singhvivek2632@gmail.com>
2026-08-18 23:52:09 +0530

    Time: 3 ms (95.85%) | Memory: 52.2 MB (55.47%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -6,7 +6,7 @@ class Solution {
             if (nums[r] == 0) {
                 zeroes++;
             }
-            while (zeroes > k) {
+            if(zeroes > k) {
                 if (nums[l] == 0) {
                     zeroes--;
                 }
