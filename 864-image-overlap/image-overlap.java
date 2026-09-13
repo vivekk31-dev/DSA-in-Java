33526f35bf0c9bf37b793d09616a4332668c31a6
Vivek Singh <singhvivek2632@gmail.com>
2026-09-13 22:10:55 +0530

    Time: 55 ms (73.86%) | Memory: 44.1 MB (94.70%) - LeetSync

---
+++ 864-image-overlap/image-overlap.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int max = 0;
+        for (int dr = -n + 1; dr < n; dr++) {
+            for (int dc = -n + 1; dc < n; dc++) {
+                int count = 0;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        int ni = i + dr;
+                        int nj = j + dc;
+                        if (ni >= 0 && ni < n &&
+                                nj >= 0 && nj < n) {
+
+                            if (img1[i][j] == 1 && img2[ni][nj] == 1) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+                max = Math.max(max, count);
+            }
+        }
+
+        return max;
+    }
+}
